54cd1a45b8bdfe6d695f387e928b6dea5978727f
William Gavin Lacalle <digitalbros7@gmail.com>
2021-01-30 00:55:58 -0600

    Adds Cat Controller and some functions

---
+++ Assets/Scripts/Cat/CatController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public List<Transform> hidingSpots = new List<Transform>();
+    private Transform currentHidingSpot = null;
+
+    public List<AudioClip> meowClips = new List<AudioClip>();
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChooseNewLocation()
+    {
+        Transform oldHidingSpot = null;
+        if (currentHidingSpot != null)
+        {
+            oldHidingSpot = currentHidingSpot;
+        }
+
+        int randNumb = Random.Range(0, hidingSpots.Count);
+        
+        transform.position = hidingSpots[randNumb].position;
+        currentHidingSpot = hidingSpots[randNumb];
+        hidingSpots.Remove(hidingSpots[randNumb]);
+
+        if (oldHidingSpot != null)
+        {
+            hidingSpots.Add(oldHidingSpot);
+        }
+    }
+
+    public void Meow()
+    {
+        audioSource.clip = meowClips[Random.Range(0, meowClips.Count)];
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Meow();
+        }
+
+        Debug.Log(hidingSpots.Count);
+    }
+}
