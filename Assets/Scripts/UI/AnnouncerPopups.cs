1177e38d0481abf307ea75a8d11194e6205fcb93
William Gavin Lacalle <digitalbros7@gmail.com>
2021-01-31 08:32:18 -0600

    Finishes Announcer for now

--- Assets/Scripts/UI/AnnouncerPopups.cs
+++ Assets/Scripts/UI/AnnouncerPopups.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using BasicTools.ButtonInspector;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[SerializeField]
+[System.Serializable]
 public class AnnouncerText
 {
+    [TextArea]
     public string text;
     public AudioClip audioClip;
 }
@@ -12,24 +14,46 @@ public class AnnouncerText
 public class AnnouncerPopups : MonoBehaviour
 {
     public List<AnnouncerText> announcerTexts = new List<AnnouncerText>();
+    private Randomizer<AnnouncerText> randomizer;
     
     public TMPro.TextMeshProUGUI textToAssign;
     private AudioSource audioSource;
     private Animator anim;
 
+    [ButtonAttribute("Show Announcement", "ShowAnnouncement")] [SerializeField]
+    private bool _btnShowAnnouncement;
+
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
+
+        randomizer = new Randomizer<AnnouncerText>(announcerTexts);
     }
 
     public void ShowAnnouncement()
     {
-        int randNumb = Random.Range(0, announcerTexts.Count);
+        if (anim.GetBool("Announcing"))
+        {
+            anim.SetTrigger("MoveOn");
+        }
+        else
+        {
+            anim.SetBool("Announcing", true);
+        }
+    }
 
-        textToAssign.text = announcerTexts[randNumb].text;
-        audioSource.clip = announcerTexts[randNumb].audioClip;
+    public void AssignVariables()
+    {
+        var announcerText = randomizer.GetRandomItem();
+
+        textToAssign.text = announcerText.text;
+        audioSource.clip = announcerText.audioClip;
         audioSource.Play();
-        //anim.fjfjf
+    }
+
+    public void SetAnnouncingFalse()
+    {
+        anim.SetBool("Announcing", false);
     }
 }
