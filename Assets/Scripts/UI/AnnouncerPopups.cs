d8ff867b2cea083eb99542018e3049fd5d6b42c7
William Gavin Lacalle <digitalbros7@gmail.com>
2021-01-31 06:52:55 -0600

    Updates hiding, adds music, Begins announcer script

---
+++ Assets/Scripts/UI/AnnouncerPopups.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class AnnouncerText
+{
+    public string text;
+    public AudioClip audioClip;
+}
+
+public class AnnouncerPopups : MonoBehaviour
+{
+    public List<AnnouncerText> announcerTexts = new List<AnnouncerText>();
+    
+    public TMPro.TextMeshProUGUI textToAssign;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ShowAnnouncement()
+    {
+        int randNumb = Random.Range(0, announcerTexts.Count);
+
+        textToAssign.text = announcerTexts[randNumb].text;
+    }
+}
