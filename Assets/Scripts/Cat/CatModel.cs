7e0f896a15febb9b2291f9fb04dd851a1f10fac2
William Gavin Lacalle <digitalbros7@gmail.com>
2021-01-30 20:11:36 -0600

    Adds a way for cats to change into cars

---
+++ Assets/Scripts/Cat/CatModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatModel : MonoBehaviour
+{
+    public GameObject catHiding;
+    public GameObject catRunning;
+
+    public Animator currentAnim;
+
+    public void SwitchCurrentModel()
+    {
+        if (catHiding.activeInHierarchy)
+        {
+            currentAnim = catHiding.GetComponent<Animator>();
+        }
+        else
+        {
+            currentAnim = catRunning.GetComponent<Animator>();
+        }
+
+        catHiding.SetActive(!catHiding.activeInHierarchy);
+        catRunning.SetActive(!catRunning.activeInHierarchy);
+    }
+
+    public void SetHidingAvatar()
+    {
+        currentAnim = catHiding.GetComponent<Animator>();
+        catHiding.SetActive(true);
+        catRunning.SetActive(false);
+    }
+
+    public void SetRunningAvatar()
+    {
+        currentAnim = catRunning.GetComponent<Animator>();
+        catRunning.SetActive(true);
+        catHiding.SetActive(false);
+    }
+}
