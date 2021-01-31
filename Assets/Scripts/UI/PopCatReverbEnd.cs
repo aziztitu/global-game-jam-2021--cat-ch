edc495c69893816fded52a77c4f02424cfd1f76c
William Gavin Lacalle <digitalbros7@gmail.com>
2021-01-31 13:52:09 -0600

    Updates hud and end screen

--- Assets/Scripts/UI/PopCatReverbEnd.cs
+++ Assets/Scripts/UI/PopCatReverbEnd.cs
@@ -1,12 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PopCatReverbEnd : MonoBehaviour
 {
+    public float maxMusicVolume = 0;
+
+    void Start()
+    {
+        maxMusicVolume = BackgroundMusic.Instance.audioSource.volume;
+    }
+
+    public void FadeOutMusic()
+    {
+        BackgroundMusic.Instance.audioSource.volume = 0;
+        //BackgroundMusic.Instance.audioSource.DOFade(0, 0.5f);
+    }
+
+    public void FadeInMusic()
+    {
+        BackgroundMusic.Instance.audioSource.volume = maxMusicVolume;
+        //BackgroundMusic.Instance.audioSource.DOFade(maxMusicVolume, 0.5f);
+    }
+
     public void PlayReverb()
     {
         SoundEffectsManager.Instance.Play("PopReverb");
         HUDScreenManager.Instance.showEndStats();
+    }
+
+    public void PlayBoing()
+    {
+        SoundEffectsManager.Instance.Play("CatAppear");
     }
 }
